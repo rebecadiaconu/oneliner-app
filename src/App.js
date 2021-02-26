911667e8221c471a6d5f38801ea5975bbbf5fddf
Rebeca Diaconu <rebeca.diaconu15@s.unibuc.ro>
2021-02-26 03:13:58 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import ForceGraph2D from 'react-force-graph-2d';
+import react, { useState } from "react";
+import data from "./data";
+import Heading from "./Components/Heading";
+import SiteGraph from "./Components/SiteGraph";
 import './App.css';
 
 function App() {
+
+    const [rootIndex, setRootIndex] = useState(0);
+    const [currentNode, setCurrentNode] = useState(data[rootIndex]);
+
+    const graphNodes = currentNode.ramuri.map((node, index) => {
+      return {
+        "id": index,
+        "name": data[data.findIndex(x => x.link === node)].title,
+        "nodeVal": node,
+        "nodeLabel": node
+      }
+    });
+
+    const graphLinks = graphNodes.map(node => {
+      if (node.id != graphNodes.length - 1) {
+        return {
+          "source": graphNodes.length - 1,
+          "target": node.id
+        }
+      }
+    }).slice(0, -1);
+
+    // console.log(graphLinks);
+
+    const dataGraph = {
+      "nodes": graphNodes,
+      "links": graphLinks
+    }
+
+    console.log(dataGraph);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <ForceGraph2D 
+        graphData={dataGraph} 
+        nodeRelSize={10}
+        backgroundColor="linen"
+        height={800}
+      />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,60 @@
+const data = [
+	{
+		"link": "www.ex1.com",
+		"title": "titlu1",
+    	"ramuri": ["www.ex3.com", "www.ex5.com", "www.ex8.com", "www.ex1.com"],
+    	"summary": "asdasdasda",
+    	"key_words": ["uagsdu", "asdasd"]
+	},
+ 	{
+		"link": "www.ex2.com",
+		"title": "titlu2",
+		"ramuri": ["www.ex3.com", "www.ex6.com", "www.ex5.com", "www.ex2.com"],
+    	"summary": "asdasdasda",
+    	"key_words": ["aaaaaaa", "bbbb"]
+	},
+	{
+		"link": "www.ex3.com",
+		"title": "titlu3",
+		"ramuri": ["www.ex1.com", "www.ex2.com", "www.ex7.com", "www.ex3.com"],
+    	"summary": "asdasdasda",
+    	"key_words": ["aaaaaaa", "bbbb"]
+	},
+	{
+		"link": "www.ex4.com",
+		"title": "titlu4",
+		"ramuri": ["www.ex7.com", "www.ex4.com"],
+    	"summary": "asdasdasda",
+    	"key_words": ["aaaaaaa", "bbbb"]
+	},
+	{
+		"link": "www.ex5.com",
+		"title": "titlu5",
+		"ramuri": ["www.ex1.com", "www.ex2.com", "www.ex5.com"],
+    	"summary": "asdasdasda",
+    	"key_words": ["aaaaaaa", "bbbb"]
+	},
+	{
+		"link": "www.ex6.com",
+		"title": "titlu6",
+		"ramuri": ["www.ex2.com", "www.ex8.com", "www.ex6.com"],
+    	"summary": "asdasdasda",
+    	"key_words": ["aaaaaaa", "bbbb"]
+	},
+	{
+		"link": "www.ex7.com",
+		"title": "titlu7",
+		"ramuri": ["www.ex4.com", "www.ex3.com", "www.ex7.com"],
+    	"summary": "asdasdasda",
+    	"key_words": ["aaaaaaa", "bbbb"]
+	},
+	{
+		"link": "www.ex8.com",
+		"title": "titlu8",
+		"ramuri": ["www.ex1.com", "www.ex6.com", "www.ex8.com"],
+    	"summary": "asdasdasda",
+    	"key_words": ["aaaaaaa", "bbbb"]
+	}
+];
+
+export default data;
